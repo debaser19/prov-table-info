60c7979163ed89b816e85b8a403a2923fc0b1de7
debaser19 <steve@stoveld.xyz>
2021-04-13 16:50:28 -0400

    added warehouse selector and sidebar

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import streamlit as st
 import auth
 
 #TODO: need to move layout to new function
-def get_dhcp_option(dashboard, vlan):
-    vlans = dashboard.appliance.getNetworkApplianceVlans(auth.buf_lab_network)
+def get_dhcp_option(dashboard, warehouse, vlan):
+    vlans = dashboard.appliance.getNetworkApplianceVlans(warehouse)
     dhcp_option = 'UNSET'
     for v in vlans:
         if v['id'] == vlan and 'dhcpOptions' in v:
@@ -23,20 +23,20 @@ def get_dhcp_option(dashboard, vlan):
                 'value': dhcp_text
             }
         ]
-        dashboard.appliance.updateNetworkApplianceVlan(auth.buf_lab_network, vlan, dhcpOptions=new_dhcp_option)
+        dashboard.appliance.updateNetworkApplianceVlan(warehouse, vlan, dhcpOptions=new_dhcp_option)
         st.write(f'Table {vlan} has been set to {dhcp_text}')
     else:
-        dashboard.appliance.updateNetworkApplianceVlan(auth.buf_lab_network, vlan, dhcpOptions=[])
+        dashboard.appliance.updateNetworkApplianceVlan(warehouse, vlan, dhcpOptions=[])
         st.write(f'Table {vlan} has been unset')
 
 
-def get_clients(dashboard, net):
+def get_clients(dashboard, warehouse):
        
-    return dashboard.networks.getNetworkClients(net, -1)
+    return dashboard.networks.getNetworkClients(warehouse, -1)
 
 
-def list_clients(dashboard, vlan):    
-    clients = [c for c in get_clients(dashboard, auth.buf_lab_network) if c['vlan'] == vlan and c['status'] == 'Online']
+def list_clients(dashboard, warehouse, vlan):    
+    clients = [c for c in get_clients(dashboard, warehouse) if c['vlan'] == vlan and c['status'] == 'Online']
     client_df = pd.DataFrame.from_records(clients)
     
     try:
@@ -49,13 +49,28 @@ def main():
     # define dashboard
     dashboard = meraki.DashboardAPI(auth.api_key, suppress_logging=True)
 
-    # get buf clients
-    table = st.selectbox(
-        'Select a table',
-        (list(range(101,109)))
+    # Warehouse selector
+    warehouse = st.sidebar.selectbox(
+        'Select a Warehouse',
+        ['BUF', 'RNO']
     )
-    get_dhcp_option(dashboard, table)
-    list_clients(dashboard, table)
+
+    if warehouse == 'BUF':
+        warehouse = auth.buf_lab_network
+        table = st.sidebar.selectbox(
+            'Select a table',
+            (list(range(101,109)))
+        )
+    else:
+        warehouse = auth.rno_lab_network
+        table = st.sidebar.selectbox(
+            'Select a table',
+            (list(range(201,208)))
+        )
+
+    # get buf clients
+    get_dhcp_option(dashboard, warehouse, table)
+    list_clients(dashboard, warehouse, table)
 
 
 if __name__ == '__main__':
